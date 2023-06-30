1181c45f80ee7df0fbaf8b544137f3a59ba10c58
Vaibhav Singh <vaibhav.singh2596@gmail.com>
2023-06-30 14:51:05 +0530

    Current progress

--- main.py
+++ main.py
@@ -6,8 +6,28 @@ my_screen.setup(width=600, height=600)
 my_screen.bgcolor("black")
 my_screen.title("Snake-Game")
 
-# Creating the Snake body
-initial_length = 3
-body_pieces = [Turtle("square") for _ in range(initial_length)]
+class Snake:
+    def __init__(self, current_screen, current_turtle):
+        self.current_screen = current_screen
+        self.my_turtle = current_turtle
+
+    def move_left(self):
+        self.my_turtle.tiltangle(90)
+
+    def move_right(self):
+        """Ti the turtle back by 10 steps"""
+        self.my_turtle.tiltangle(-90)
+
+    def snake_body(self):
+        # Creating the Snake body
+        initial_length = 3
+        x_value = 0
+        y_value = 0
+        for _ in range(initial_length):
+            new_turtle = Turtle("square")
+            new_turtle.penup()
+            new_turtle.setpos(x_value, y_value)
+            x_value -= 20
+
 
 my_screen.exitonclick()
